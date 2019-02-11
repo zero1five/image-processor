5b2dcd7982e955862d81675dbc7ea8bbe99dec6a
zero1five <zerodengyin@gmail.com>
2019-02-11 21:45:17 +0800

    feat: add canvas drawImage

--- application/App.js
+++ application/App.js
@@ -4,7 +4,7 @@ import ImageProcessor from '../src';
 export default class App extends PureComponent {
   render() {
     return (
-      <div style={{width: 600, height: 350, margin: '300px auto 0px'}}>
+      <div style={{width: 300, height: 175, margin: '300px auto 0px'}}>
         <ImageProcessor />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,19 +1,52 @@
 import React, {Component} from 'react';
-import './index.css';
+import './style/index.css';
+
+/**
+ * 点击加号添加图片到对应的canvas渲染区域
+ *
+ */
 
 class ImageProcessor extends Component {
+  constructor(props) {
+    super(props);
+    this.input = React.createRef();
+  }
+
+  handleFiles = ev => {
+    const reader = new FileReader();
+    reader.readAsDataURL(ev.target.files[0]);
+    reader.onload = e => {
+      this.drawCanvas(e.target.result);
+    };
+  };
+
+  drawCanvas = imgData => {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+
+    const img = new Image();
+    img.src = imgData;
+    img.onload = function() {
+      context.drawImage(img, 0, 0);
+    };
+  };
+
   render() {
     return (
       <div className="pro-container">
-        <ProContent />
+        <div className="pro-content" onClick={() => this.input.current.click()}>
+          +
+        </div>
+        <canvas id="canvas" className="pro-canvas-mask" />
+        <input
+          ref={this.input}
+          accept="image/*"
+          type="file"
+          style={{display: 'none'}}
+          onChange={this.handleFiles}
+        />
       </div>
     );
-  }
-}
-
-class ProContent extends Component {
-  render() {
-    return <div className="pro-content">+</div>;
   }
 }
 
